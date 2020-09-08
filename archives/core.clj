d1590f7def7b7cd26ccc7ad8f8570c44121edd3c
lcol3117 <59921084+lcol3117@users.noreply.github.com>
2020-09-08 15:14:59 -0400

    Update core.clj

--- archives/core.clj
+++ archives/core.clj
@@ -3,23 +3,28 @@
     :as a
     :refer [>! <! >!! <!! go chan buffer close! thread alts! alts!! timeout]]))
 
-(defn archive [in-chan-args out-chan-args flow-map]
+(defn archive [in-chan-args out-chan-args flow-map-given]
   (do
     (def in-chan (apply chan in-chan-args))
-    (def out-chan-map (into (hash-map)
-      (map (fn [[key_ _]] [key_ (apply chan out-chan-args)]) (assoc flow-map :else #(true)))))
-    (go-loop [_ nil]
-      ; run archive process
-      (recur nil))
+    (def out-chan (apply chan out-chan-args))
+    (let [flow-map flow-map-given]
+      (go-loop [_ nil]
+        ; run archive process
+        (recur nil)))
     {
       :in-chan in-chan
-      :out-chan-map out-chan-map}))
+      :out-chan out-chan}))
 
 (defn give!! [archive v]
-  (>!! (archive :in-chan) v)))
+  (>!! (archive :in-chan) {
+                            :task :store
+                            :data v}))
 
 (defn retrieve!! [archive category]
   (let [
          t (thread
-             (<!! ((archive :out-chan-map) category)))]
+          (>!! (archive :in-chan) {
+                                    :task :locate
+                                    :data category})
+          (<!! (archive :out-chan)))]
     (<!! t)))
