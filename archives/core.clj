4ebf5860cb337351182acad26f602365bfbe42f0
lcol3117 <59921084+lcol3117@users.noreply.github.com>
2020-09-09 09:13:37 -0400

    almost done

--- archives/core.clj
+++ archives/core.clj
@@ -1,15 +1,20 @@
 (ns archives.core
   (:require [clojure.core.async
     :as a
-    :refer [>! <! >!! <!! go chan buffer close! thread alts! alts!! timeout]]))
+    :refer [>! <! >!! <!! go chan thread]]))
 
 (defn archive [in-chan-args pass-chan-args flow-map-given]
   (do
     (def in-chan (apply chan in-chan-args))
     (def pass-chan (apply chan pass-chan-args))
-    (go-loop [_ nil]
-      ; run archive process
-      (recur nil))
+    (go-loop [curr-archive []]
+      (def new-given (<! in-chan))
+      (def novelty
+        (if (= (new-given :task) :store)
+          (new-given :data)))
+      (if (= (new-given :task) :retrieve)
+        (>! (new-given :data) curr-archive))
+      (if novelty (recur (conj curr-archive novelty)) (recur curr-archive)))
     {
       :in-chan in-chan
       :pass-chan pass-chan}))
