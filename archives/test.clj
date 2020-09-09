e42702d1043c3218ae00862629ce864be32fe899
lcol3117 <59921084+lcol3117@users.noreply.github.com>
2020-09-09 09:46:29 -0400

    Update test.clj

--- archives/test.clj
+++ archives/test.clj
@@ -1,1 +1,12 @@
+(ns archives.test
+  (:require archives.core :as archv
+    :refer [>>!! <<!! archive]))
 
+(def my-archive (archive [] []))
+
+(>>!! my-archive :first)
+(>>!! my-archive :second)
+
+(<<!! my-archive)
+
+(assert (= (<<!! my-archive) [:first :second]))
